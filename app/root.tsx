b17c0d1a49552ae8664a3461cd5b63953e877c9c
arnokay <arnokay.work@gmail.com>
2025-06-17 02:29:45 +0300

    feat: alright, at one point gonna fix this, right now we stay with flashbang

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-muted">
+      <body>
         {children}
         <ScrollRestoration />
         <Scripts />
