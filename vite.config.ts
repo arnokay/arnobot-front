7de1aaa9a35eae557d2f038b5a01c86485c2b4aa
arnokay <arnokay.work@gmail.com>
2025-06-17 19:41:24 +0300

    feat: change color scheme, add different icon library, update login form

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()], 
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 });
