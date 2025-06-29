09630134a2d97c42e461418916616941b5d89528
arnokay <arnokay.work@gmail.com>
2025-06-29 05:00:41 +0300

    feat: add support for custom name in AppName

--- app/components/app-name.tsx
+++ app/components/app-name.tsx
@@ -1,7 +1,18 @@
+import config from "~/config";
+
 export default function AppName() {
+  const appName = config.app.name;
+  let firstPart: string = '';
+  let secondPart: string = '';
+  if (appName.length <= 3) {
+    secondPart = appName
+  } else {
+    firstPart = appName.slice(0, appName.length - 3)
+    secondPart = appName.slice(-3)
+  }
   return (
     <span className="font-bold">
-      arno<span className="text-primary">bot<span className="animate-[blink_2s_ease-in-out_infinite] [animation-fill-mode:both]">_</span></span>
+      {firstPart}<span className="text-primary">{secondPart}<span className="animate-[blink_2s_ease-in-out_infinite] [animation-fill-mode:both]">_</span></span>
     </span>
   );
 }
